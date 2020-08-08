815eaa7a91996c299455c9733cfc6cbab5359223
Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2020-08-08 12:59:21 +0530

    all variables required

---
+++ config.js
@@ -0,0 +1,24 @@
+const requiredEnvVariables = [
+  'JWT_SECRET_KEY',
+  'MONGODB_URL',
+  'CLOUD_NAME',
+  'API_KEY',
+  'API_SECRET',
+];
+
+const missingEnvVariables = requiredEnvVariables.filter(
+  (envVar) => !process.env[envVar]
+);
+
+if (missingEnvVariables.length) {
+  throw new Error(`⚠ Missing environment variables ${missingEnvVariables}`);
+}
+
+module.exports = {
+  jwtSecretKey: process.env.JWT_SECRET_KEY,
+  port: process.env.PORT || 5000,
+  mongoDBUrl: process.env.MONGODB_URL,
+  cloudName: process.env.CLOUD_NAME,
+  apiKey: process.env.API_KEY,
+  apiSecret: process.env.API_SECRET,
+};
